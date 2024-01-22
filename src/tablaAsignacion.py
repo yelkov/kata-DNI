745f26ef9511677cd80cbc0f2827a898a2bbc4e4
AmarinoLama <amarinolama@gmail.com>
2024-01-22 10:32:22 +0100

    feat(tablaAsignacion.py): añadir funcion __init__

---
+++ src/tablaAsignacion.py
@@ -0,0 +1,28 @@
+class tablaAsignacion:
+    
+    def __init__(self):
+        self.tabla = [  
+            "T",
+            "R",
+            "W",
+            "A",
+            "G",
+            "M",
+            "Y",
+            "F",
+            "P",
+            "D",
+            "X",
+            "B",
+            "N",
+            "J",
+            "Z",
+            "S",
+            "Q",
+            "V",
+            "H",
+            "L",
+            "C",
+            "K",
+            "E",
+        ]
