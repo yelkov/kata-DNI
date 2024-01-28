0b93db8b8572dddaf2af794ff980bcd920c282da
Yelko Veiga <yelkovq@gmail.com>
2024-01-28 21:19:00 +0100

    refactor(dni): refactoring checkLetter n adding getLetter

--- src/DNI.py
+++ src/DNI.py
@@ -8,18 +8,21 @@ class Dni:
         self.tabla = tablaAsignacion()
         self.dni = dni
     
-    def checkLen(self):
-        return len(self.dni) == self.LENDNI
-    
     def getNumbers(self):
         try:
             return int(self.dni[:8])
         except ValueError:
             return None 
-            
+    
+    def getLetter(self):
+        return self.dni[-1]
+
+    def checkLen(self):
+        return len(self.dni) == self.LENDNI        
+
     def checkNumbers(self):
         return  True if self.getNumbers() else False
 
     def checkLetter(self):
-        return self.dni[-1] in self.tabla.getTabla()
+        return self.getLetter() in self.tabla.getTabla()
     
