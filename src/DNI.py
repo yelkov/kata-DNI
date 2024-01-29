2f180ba328ebc6fb0d51cc20b5e6a5138b2a802a
YelkoVeiga <yelkovq@gmail.com>
2024-01-29 09:56:50 +0100

    refactor(Dni): changing funcion names

--- src/DNI.py
+++ src/DNI.py
@@ -2,7 +2,8 @@ from src.tablaAsignacion import tablaAsignacion
 
 class Dni:
 
-    LENDNI = 9
+    LENGHT_DNI = 9
+    LENGHT_DNI_NUMBERS = 8
 
     def __init__(self, dni):
         self.tabla = tablaAsignacion()
@@ -10,25 +11,25 @@ class Dni:
     
     def getNumbers(self):
         try:
-            return int(self.dni[:8])
+            return int(self.dni[:self.LENGHT_DNI_NUMBERS])
         except ValueError:
             return None 
     
     def getLetter(self):
         return self.dni[-1]
 
-    def checkLen(self):
-        return len(self.dni) == self.LENDNI        
+    def isValidLenght(self):
+        return len(self.dni) == self.LENGHT_DNI        
 
     def checkNumbers(self):
-        return  True if self.getNumbers() else False
+        return  self.getNumbers() != None
 
     def checkLetter(self):
         return self.getLetter() in self.tabla.getTabla()
     
     def checkDni(self):
         return (
-             self.checkLen() and 
+             self.isValidLenght() and 
              self.checkNumbers() and 
              self.checkLetter() and 
              self.getLetter() == self.tabla.calculateLetter(self.getNumbers())
--- test/test_dni.py
+++ test/test_dni.py
@@ -2,11 +2,11 @@ import pytest
 from src.DNI import Dni
 
 
-@pytest.mark.checkLen
-def test_checkLen():
-    assert Dni("12312312A").checkLen()
-    assert not Dni("123123ASFSD12A").checkLen()
-    assert not Dni("1231").checkLen()
+@pytest.mark.isValidLenght
+def test_isValidLenght():
+    assert Dni("12312312A").isValidLenght()
+    assert not Dni("123123ASFSD12A").isValidLenght()
+    assert not Dni("1231").isValidLenght()
 
 @pytest.mark.checkNumbers
 def test_checkNumbers():
