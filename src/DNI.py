397224baec248c965f1215bda10697f5e03c61b7
Yelko Veiga <yelkovq@gmail.com>
2024-01-30 19:37:18 +0100

    refactor(dni): refactoring private and public

--- src/DNI.py
+++ src/DNI.py
@@ -2,18 +2,23 @@ from src.tablaAsignacion import tablaAsignacion
 
 class Dni:
 
-    LENGHT_DNI = 9
-    LENGHT_DNI_NUMBERS = 8
-
     def __init__(self, dni):
         self.tabla = tablaAsignacion()
         self.dni = dni
+        self.LENGHT_DNI = 9
+        self.LENGHT_DNI_NUMBERS = 8
         self.numbers = dni[:self.LENGHT_DNI_NUMBERS]
         self.letter = dni[-1]
 
 
     def getDni(self):
         return self.dni
+
+    def getNumbers(self):
+        return int(self.numbers) if self.isValidNumber() else 'El número del DNI introducido no es válido.'
+    
+    def getLetter(self):
+        return self.letter if self.isValidLetter() else 'La letra del DNI introducido no es válida.'
     
     def checkDni(self):
         return (
@@ -22,18 +27,14 @@ class Dni:
              self.isValidLetter()
              )
     
+    ##########  PARTE PRIVADA   ########## 
+
     def isValidNumber(self):
         try:
             int(self.numbers)
             return True
         except ValueError:
             return False
-
-    def getNumbers(self):
-        return int(self.numbers) if self.isValidNumber() else 'El número del DNI introducido no es válido.'
-    
-    def getLetter(self):
-        return self.dni[-1] if self.isValidLetter() else 'La letra del DNI introducido no es válida.'
 
     def isValidLenght(self):
         return len(self.dni) == self.LENGHT_DNI        
